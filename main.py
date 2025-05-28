e0692b345ed3635a0972729ca7797cbdd5ed91af
David Woodard <121324176+drwoodard72@users.noreply.github.com>
2025-05-27 21:07:17 -0500

    CH3:Report - L1:Print Report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from stats import get_num_words
+from stats import get_word_count
 from stats import get_num_characters
+from stats import get_sorted_char_counts
 
 def get_book_text(path_to_file):
 
@@ -7,13 +8,20 @@ def get_book_text(path_to_file):
         return f.read()
 
 def main():
-    book_file_path="./books/frankenstein.txt"
+    book_file_path="books/frankenstein.txt"
     book_text = get_book_text(book_file_path)
-    book_word_count = get_num_words(book_text)
-    book_character_counts = get_num_characters(book_text)
+    book_word_count = get_word_count(book_text)
+    sorted_char_counts = get_sorted_char_counts(get_num_characters(book_text))
 
-    print(book_word_count)
-    print(book_character_counts)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {book_word_count} total words")
+    print("--------- Character Count -------")
+    for char_count in sorted_char_counts:
+        if char_count["char"].isalpha() == True:        
+            print(f"{char_count["char"]}: {char_count["num"]}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,9 @@
+def get_word_count(text):
+    return len(text.split())
+
 def get_num_words(text):
-    return f"{len(text.split())} words found in the document"
+    return f"{get_word_count(text)} words found in the document"
+
 
 def get_num_characters(text):
     chars = {}
@@ -10,3 +14,15 @@ def get_num_characters(text):
             chars[c.lower()] = 1
 
     return chars
+
+def sort_func(dict):
+    return dict["num"]
+
+def get_sorted_char_counts(chars):
+    results = []
+
+    for char in chars:
+        results.append({"char":char,"num":chars[char]})
+    
+    results.sort(key=sort_func, reverse=True)
+    return results
