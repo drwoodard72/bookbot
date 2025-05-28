9aa5864f6c4ed7f2c67379454c7f487423c8245d
David Woodard <121324176+drwoodard72@users.noreply.github.com>
2025-05-27 20:26:14 -0500

    CH2:Data Analysis - L5 Refactor

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+from stats import get_num_words
+
 def get_book_text(path_to_file):
 
     with open(path_to_file) as f:
         return f.read()
 
-def get_word_count(text):
-    return f"{len(text.split())} words found in the document"
-
 def main():
     book_file_path="./books/frankenstein.txt"
     #print(get_book_text(book_file_path))
-    print(get_word_count(get_book_text(book_file_path)))
+    print(get_num_words(get_book_text(book_file_path)))
 
 main()
---
+++ stats.py
@@ -0,0 +1,2 @@
+def get_num_words(text):
+    return f"{len(text.split())} words found in the document"
