c138b6ec2572f88045c0eb99d95b5a1496e7fdb7
David Woodard <121324176+drwoodard72@users.noreply.github.com>
2025-05-29 11:08:55 -0500

    CH3:Report - L2:Arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_word_count
 from stats import get_num_characters
 from stats import get_sorted_char_counts
+import sys
 
 def get_book_text(path_to_file):
 
@@ -8,7 +9,11 @@ def get_book_text(path_to_file):
         return f.read()
 
 def main():
-    book_file_path="books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_file_path=sys.argv[1]
     book_text = get_book_text(book_file_path)
     book_word_count = get_word_count(book_text)
     sorted_char_counts = get_sorted_char_counts(get_num_characters(book_text))
